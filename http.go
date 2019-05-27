61e2f3aa30363b83910c3677c8e4a5bde92b4f7b
Alex Krantz <alex@alexkrantz.com>
2019-05-27 00:27:28 -0700

    Add record filtering

--- http.go
+++ http.go
@@ -676,6 +676,29 @@ func list(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "GET" {
 		util.Responses.Error(w, http.StatusMethodNotAllowed, "method not allowed")
 		return
+	} else if len(r.URL.RawQuery) != 0 {
+		if _, ok := r.URL.Query()["type"]; !ok {
+			util.Responses.Error(w, http.StatusBadRequest, "query parameter 'type' is required for type filtering")
+			return
+		}
+
+		records := make(map[string][]string)
+		for _, record := range r.URL.Query()["type"] {
+			records[record] = []string{}
+			if err := db.View(func(tx *bbolt.Tx) error {
+				return tx.Bucket([]byte(record)).ForEach(func(k, v []byte) error {
+					records[record] = append(records[record], strings.Split(string(k), "*")[0])
+					return nil
+				})
+			}); err != nil {
+				util.Responses.Error(w, http.StatusInternalServerError, "failed to retrieve all records: " + err.Error())
+				return
+			}
+			records[record] = util.RemoveDuplicates(records[record])
+		}
+
+		util.Responses.SuccessWithData(w, records)
+		return
 	}
 
 	records := map[string][]string{
