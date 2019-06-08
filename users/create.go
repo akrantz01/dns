052d13586169525f4760a7ea46a64f849d418213
Alex Krantz <alex@alexkrantz.com>
2019-06-08 16:05:48 -0700

    Require admin privileges to create users

--- users/create.go
+++ users/create.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/akrantz01/krantz.dev/dns/db"
 	"github.com/akrantz01/krantz.dev/dns/util"
+	"github.com/dgrijalva/jwt-go"
 	bolt "go.etcd.io/bbolt"
 	"net/http"
 )
@@ -19,6 +20,36 @@ func create(w http.ResponseWriter, r *http.Request, database *bolt.DB) {
 		return
 	} else if r.Header.Get("Content-Type") != "application/json" {
 		util.Responses.Error(w, http.StatusBadRequest, "body must be of type JSON")
+		return
+	} else if r.Header.Get("Authorization") == "" {
+		util.Responses.Error(w, http.StatusUnauthorized, "header 'Authorization' is required")
+		return
+	}
+
+	// Verify JWT in headers
+	token, err := db.TokenFromString(r.Header.Get("Authorization"), database)
+	if err != nil {
+		util.Responses.Error(w, http.StatusUnauthorized, "failed to authenticate: "+err.Error())
+		return
+	}
+
+	// Get username from token
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		util.Responses.Error(w, http.StatusBadRequest, "invalid JWT claims format")
+		return
+	}
+
+	// Get user from token
+	user, err := db.UserFromDatabase(claims["sub"].(string), database)
+	if err != nil {
+		util.Responses.Error(w, http.StatusUnauthorized, "failed to retrieve user: "+err.Error())
+		return
+	}
+
+	// Check role
+	if user.Role != "admin" {
+		util.Responses.Error(w, http.StatusForbidden, "user must be of role 'admin'")
 		return
 	}
 
