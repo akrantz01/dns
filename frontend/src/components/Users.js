92604dada2102b4bdf75bbdf3b330728224ee848
Alex Krantz <alex@alexkrantz.com>
2019-06-15 13:45:21 -0700

    Add header to actions

--- frontend/src/components/Users.js
+++ frontend/src/components/Users.js
@@ -191,7 +191,7 @@ export default class extends Component {
                 sortable: true
             },
             {
-                field: "Actions",
+                name: "Actions",
                 actions: [
                     {
                         name: "Edit",
